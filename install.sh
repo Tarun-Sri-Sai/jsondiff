6c0343c40efd59f7dab54520a493cfdd854b7572
Tarun <tarunsrisaiv03@gmail.com>
2024-06-25 20:25:29 +0530

    Fix install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ cd jsondiff
 git checkout release
 cargo build --release
 
-$destDirectory = '/usr/local/bin'
+destDirectory='/usr/local/bin'
 echo "Moving binary to $destDirectory."
 sudo mkdir -p $destDirectory
-mv ./target/release/jsondiff $destDirectory
+sudo mv ./target/release/jsondiff $destDirectory
 
 echo Cleaning up.
 cd ..
